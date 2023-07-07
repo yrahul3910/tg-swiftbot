45f442049d0bcc32b9eed460149fc9a35a54a3b8
Rahul Yedida <rahul.yedida@pm.me>
2023-07-07 02:04:31 -0400

    Added basic functionality

---
+++ acronyms.py
@@ -0,0 +1,44 @@
+acronyms = {
+    # Speak Now (Taylor's Version)
+    "SN": "Speak Now",
+    "BTD": "Back To December",
+    "SOU": "The Story of Us",
+    "TSOU": "The Story of Us",
+    "NGU": "Never Grow Up",
+    "BTR": "Better Than Revenge",
+    "ETTVFTVFFOB": "Electric Touch (Taylor's Version) (From The Vault) [ft. Fall Out Boy]",
+    "ET": "Electric Touch (Taylor's Version) (From The Vault) [ft. Fall Out Boy]",
+    "WEFILTVFTV": "When Emma Falls in Love (Taylor's Version) (From The Vault)",
+    "WEFIL": "When Emma Falls in Love (Taylor's Version) (From The Vault)",
+    "ICSUTVFTV": "I Can See You (Taylor's Version) (From The Vault)",
+    "ICSU": "I Can See You (Taylor's Version) (From The Vault)",
+    "ICSYTVFTV": "I Can See You (Taylor's Version) (From The Vault)",
+    "ICSY": "I Can See You (Taylor's Version) (From The Vault)",
+    "CCTVFTVFHW": "Castles Crumbling (Taylor's Version) (From The Vault) [ft. Hayley Williams]",
+    "CC": "Castles Crumbling (Taylor's Version) (From The Vault) [ft. Hayley Williams]",
+    "FOTVFTV": "Foolish One (Taylor's Version) (From The Vault)",
+    "FO": "Foolish One (Taylor's Version) (From The Vault)",
+
+    # Red (Taylor's Version)
+    "IKYWT": "I Knew You Were Trouble",
+    "ATWTMVTVFTV": "All Too Well (10 Minute Version) [Taylor's Version] [From The Vault]",
+    "ATWTMV": "All Too Well (10 Minute Version)",
+    "ATWSGAVRALPS": "All Too Well (Sad Girl Autumn Version) [Recorded at Long Pond Studios]",
+    "ATW": "All Too Well",
+    "WANEGBT": "We Are Never Ever Getting Back Together",
+    "SSS": "Stay Stay Stay",
+    "TLT": "The Last Time (Taylor's Version) [ft. Gary Lightbody]",
+    "SBT": "Sad Beautiful Tragic",
+    "TLO": "The Lucky One",
+    "EHC": "Everything Has Changed",
+    "TMIK": "The Moment I Knew",
+    "CBBH": "Come Back... Be Here",
+    "GAH": "Girl At Home",
+    "SOG": "State Of Grace",
+    "MAIB": "Message In a Bottle",
+    "IBYTHAM": "I Bet You Think About Me",
+    "TVFN": "The Very First Night",
+
+    # Others
+    "CTF": "Christmas Tree Farm"
+}
---
+++ main.py
@@ -0,0 +1,90 @@
+from typing import Any, Generator
+from bot import TelegramBot
+from acronyms import acronyms
+
+
+def get_response(msg: str, multi: bool) -> Generator[tuple[str, str | None], Any, None]:
+    full = None  # Expanded form of the acronym
+    detected = ""  # Acronym detected
+    for acronym in acronyms:
+        if acronym + "TV" in msg:
+            detected = acronym + "TV"
+
+            if "Taylor's Version" not in acronyms[acronym]:
+                if "(" in msg:
+                    full = acronyms[acronym] + " [Taylor's Version]"
+                else:
+                    full = acronyms[acronym] + " (Taylor's Version)"
+            else:
+                full = acronyms[acronym]
+            
+            if multi:
+                yield detected, full
+                continue
+            else:
+                break
+        
+        if acronym in msg:
+            detected = acronym
+            full = acronyms[acronym]
+
+            if multi:
+                yield detected, full
+            else:
+                break
+    
+    yield detected, full
+
+
+def make_reply(msg: str) -> str | None:
+    if "/nobot" in msg:
+        return None
+
+    # Match all acronyms?
+    multi = False
+    list_formatter = ""
+    if "/multi" in msg:
+        multi = True
+        list_formatter = "- "
+
+    full = None
+    if msg is not None:
+        entries = list(get_response(msg, multi))
+
+        # Dedup entries
+        entries = list(set(entries))
+
+        # Compose reply message
+        reply = ""
+        for entry in entries:
+            detected, full = entry
+            if full is not None:
+                reply += f"{list_formatter}{detected} can refer to: {full}\n"
+        
+        return None if reply == "" else reply
+
+    return None
+
+
+bot = TelegramBot('config.cfg')
+print("Server started.")
+
+update_id = None
+while True:
+    updates = bot.get_updates(offset=update_id)
+    updates = updates["result"]
+    if updates:
+        for item in updates:
+            print(item)
+            update_id = item["update_id"]
+            try:
+                message = item["message"]
+                message = str(item["message"]["text"])
+                from_ = item["message"]["chat"]["id"]
+
+                reply = make_reply(message)
+
+                if reply is not None:
+                    bot.send_message(reply, from_)
+            except Exception as e:
+                print(f"Failed, item = {item}: {e}")
